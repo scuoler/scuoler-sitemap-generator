c5638fc8e5a8e844e0538ff8c519c144f29a6ada
mathew joseph <mathew_joseph31@yahoo.com>
2024-07-14 07:15:04 +0530

    commit prior to typescript conversion, added quizes/problems sitemap

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const configuration = require("../Configuration");
 const constants = require("../Constants");
 const fs = require("fs");
 
-const generateCourseSitemap = (filePath) => {
+const getPool = () => {
   const pool = new pg.Pool({
     host: configuration.getHost(),
     user: configuration.getUserId(),
@@ -15,24 +15,39 @@ const generateCourseSitemap = (filePath) => {
     ssl: { rejectUnauthorized: false },
   });
 
+  return pool;
+};
+
+const writeHeader = (stream, endOfLine) => {
+  const header =
+    `<?xml version="1.0" encoding="UTF-8"?>` +
+    endOfLine +
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">` +
+    endOfLine;
+  stream.write(header);
+};
+
+const writeFooter = (stream) => {
+  const footer = `</urlset>`;
+  stream.write(footer);
+};
+
+const generateCourseSitemap = (filePath) => {
+  const pool = getPool();
+
   let sql = `  select id from public.course where sitemap=true`;
 
   pool.query(sql, [], function (err, result, fields) {
+    pool.end(() => {});
     if (err) {
       console.log(err);
-      pool.end(() => {});
     } else {
       const courseStream = fs.createWriteStream(filePath);
-      var endOfLine = require("os").EOL;
-      const header =
-        `<?xml version="1.0" encoding="UTF-8"?>` +
-        endOfLine +
-        `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">` +
-        endOfLine;
-      courseStream.write(header);
+      const endOfLine = require("os").EOL;
+      writeHeader(courseStream, endOfLine);
       for (let i = 0; i < result.rows.length; i++) {
         let id = result.rows[i].id;
-        let rowUrl = `https://scuoler.com/courseShowSelected/${id}`;
+        let rowUrl = `https://${constants.LETSENCRYPT_DOMAIN_NAME}/courseShowSelected/${id}`;
         let ele =
           ` <url>` +
           endOfLine +
@@ -43,15 +58,80 @@ const generateCourseSitemap = (filePath) => {
         //console.log(rowUrl);
         courseStream.write(ele);
       }
-      const footer = `</urlset>`;
-      courseStream.write(footer);
+      writeFooter(courseStream);
       courseStream.close();
+    }
+  });
+};
+
+const generateQuizSitemap = (filePath) => {
+  const pool = getPool();
+
+  let sql = `  select id from public.quiz where sitemap=true`;
+
+  pool.query(sql, [], function (err, result, fields) {
+    pool.end(() => {});
+    if (err) {
+      console.log(err);
+    } else {
+      const quizStream = fs.createWriteStream(filePath);
+      var endOfLine = require("os").EOL;
+      writeHeader(quizStream, endOfLine);
+      for (let i = 0; i < result.rows.length; i++) {
+        let id = result.rows[i].id;
+        let rowUrl = `https://${constants.LETSENCRYPT_DOMAIN_NAME}/quizShowSelected/${id}`;
+        let ele =
+          ` <url>` +
+          endOfLine +
+          `  <loc>${rowUrl}</loc>` +
+          endOfLine +
+          ` </url>` +
+          endOfLine;
+        //console.log(rowUrl);
+        quizStream.write(ele);
+      }
+      writeFooter(quizStream);
+      quizStream.close();
+    }
+  });
+};
+
+const generateProblemSitemap = (filePath) => {
+  const pool = getPool();
+
+  let sql = `  select id from public.problem where sitemap=true`;
+
+  pool.query(sql, [], function (err, result, fields) {
+    pool.end(() => {});
+    if (err) {
+      console.log(err);
+    } else {
+      const problemStream = fs.createWriteStream(filePath);
+      var endOfLine = require("os").EOL;
+      writeHeader(problemStream, endOfLine);
+      for (let i = 0; i < result.rows.length; i++) {
+        let id = result.rows[i].id;
+        let rowUrl = `https://${constants.LETSENCRYPT_DOMAIN_NAME}/problemShowSelected/${id}`;
+        let ele =
+          ` <url>` +
+          endOfLine +
+          `  <loc>${rowUrl}</loc>` +
+          endOfLine +
+          ` </url>` +
+          endOfLine;
+        //console.log(rowUrl);
+        problemStream.write(ele);
+      }
+      writeFooter(problemStream);
+      problemStream.close();
     }
   });
 };
 
 const main = () => {
   generateCourseSitemap(constants.COURSE_SITEMAP_FILE_PATH);
+  generateQuizSitemap(constants.QUIZ_SITEMAP_FILE_PATH);
+  generateProblemSitemap(constants.PROBLEM_SITEMAP_FILE_PATH);
 };
 
 //cron.schedule("*/5 * * * *", main);
